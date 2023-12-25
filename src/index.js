2eb263b0debdcb31755095c71b6dcd47ecd70787
Marco <marco.malvicini@gmail.com>
2023-12-25 10:23:20 +0100

    configure eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
 
-import usersRoutes from './routes/users.js'
+import usersRoutes from './routes/users.js';
 
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
-fastify.register(usersRoutes, {prefix: '/users'})
+fastify.register(usersRoutes, { prefix: '/users' });
 
 /**
  * Run the server!
  */
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000 });
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-start()
+};
+start();
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,16 @@
-import mockedUsers from '../../mocks/users.json' assert { type: "json" }
+import mockedUsers from '../../mocks/users.json' assert { type: 'json' };
 
 export default async function (fastify) {
   fastify.get('/', async (req, res) => {
-    res.send(mockedUsers)
-  })
+    res.send(mockedUsers);
+  });
 
   fastify.get('/:id', async (req, res) => {
-    const {params: {id}} = req
-    const user = mockedUsers.find(user => user.id === id)
-    
-    res.send(user)
-  })
+    const {
+      params: { id },
+    } = req;
+    const user = mockedUsers.find((user) => user.id === id);
+
+    res.send(user);
+  });
 }
