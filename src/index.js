b0c9810463f4f2097931a8073cb1fbcaaac2125e
Marco <marco.malvicini@gmail.com>
2024-01-14 14:11:30 +0100

    add postgres

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const Fastify = require('fastify');
 
-const usersRoutes = require('./routes/users.js');
-
-const fastify = Fastify({
-  logger: true,
-});
-
-fastify.register(usersRoutes, { prefix: '/users' });
+const usersRoutes = require('./routes/users');
+const getPostgresClient = require('./connectors/postgresdb');
 
 const startServer = async () => {
+  const fastify = Fastify({
+    logger: true,
+  });
+  const dbClient = await getPostgresClient();
+
+  // Register postgres DB
+  fastify.decorate('dbClient', dbClient);
+
+  // Register routes
+  fastify.register(usersRoutes, { prefix: '/users' });
+
+  // Start server
   try {
     await fastify.listen({ port: 3000 });
   } catch (err) {
